c806b4e636ac487df869a7c60f127087aa213a46
ElkYTHN <elkinportillo4@gmail.com>
2022-11-25 20:03:21 -0600

    Listo Ultimas

--- Crypto Wallet/Crypto Wallet/Application/Constants.cs
+++ Crypto Wallet/Crypto Wallet/Application/Constants.cs
@@ -4,8 +4,8 @@ namespace Crypto_Wallet
 {
     public class Constants
     {
-        public const string TRANSACTION_DEPOSITED = "Deposited";
-        public const string TRANSACTION_WITHDRAWN = "Withdrawn";
+        public const string TRANSACTION_DEPOSITED = "Depositos";
+        public const string TRANSACTION_WITHDRAWN = "Retiros";
         public const string TRANSACTION_DEPOSITED_IMAGE = "deposited.png";
         public const string TRANSACTION_WITHDRAWN_IMAGE = "withdrawn.png";
     }
